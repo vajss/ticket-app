381836fc786f6ea5800739ce618bf77f7739f030
Vajss <vasilije1998@gmail.com>
2024-11-10 11:03:12 +0100

    remove one day

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-document.getElementById("ticketNumber1").innerText = "00" + Math.floor((subtractTime( new Date(), 2, -91).getTime() / 1000) / 67.84);
-document.getElementById("date1").innerText = formatDate(subtractTime( new Date(), 3, -82));
-document.getElementById("divider1").innerText = formatDividerDate(subtractTime( new Date(), 3, -8));
 
-document.getElementById("ticketNumber2").innerText = "00" + Math.floor((subtractTime( new Date(), 2, -91).getTime() / 1000) / 67.84);
+
+document.getElementById("ticketNumber2").innerText = "00" + Math.floor((subtractTime( new Date(), 2, -91).getTime() / 1000) / 63.84);
 document.getElementById("date2").innerText = formatDate(subtractTime( new Date(), 2, -91));
 document.getElementById("divider2").innerText = formatDividerDate(subtractTime( new Date(), 2, 9));
 
-document.getElementById("ticketNumber3").innerText = "00" + (Math.floor((subtractTime( new Date(), 1, -83).getTime() / 1000) / 67.84) + 1);
+document.getElementById("ticketNumber3").innerText = "00" + (Math.floor((subtractTime( new Date(), 1, -83).getTime() / 1000) / 63.84) + 1);
 document.getElementById("date3").innerText = formatDate(subtractTime( new Date(), 1, -91));
 document.getElementById("divider3").innerText = formatDividerDate(subtractTime( new Date(), 1, 9));
 
 // TODO check in more detail 67.84 constant
-document.getElementById("ticketNumber4").innerText = "00" + Math.floor(((new Date()).getTime() / 1000) / 67.84);
+document.getElementById("ticketNumber4").innerText = "00" + Math.floor(((new Date()).getTime() / 1000) / 63.84);
 document.getElementById("date4").innerText = formatDate(subtractTime( new Date(), 0, -85));
 document.getElementById("divider4").innerText = formatDividerTodayDate(subtractTime( new Date(), 0, 5));
 
